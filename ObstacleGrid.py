247594243fab6c51a0b3158d126249949a361695
Seasnakez <adde456789@gmail.com>
2023-08-19 23:07:22 +0200

    Changed the position of width and height. As matrix coordinates are referred in [row, column] aka [y, x], While most methods were assuming it worked as [x, y]

--- ObstacleGrid.py
+++ ObstacleGrid.py
@@ -1,21 +1,25 @@
 class ObstacleGrid:
-    def __init__(self, width, height, cell_width, cell_height):
+    def __init__(self, height, width, cell_width, cell_height):
         self.grid = [[0 for _ in range(width)] for _ in range(height)]
+        self.cell_width = cell_width
+        self.cell_height = cell_height
+        self.height = height
+        self.width = width
 
-        def adjacent(self, include_obstacles=True):
-            pass
+    def adjacent(self, include_obstacles=True):
+        pass
 
-        def toggle_obstacle(x, y):
-            if self.grid[y][x] == 0:
-                self.grid[y][x] = 1
-            else:
-                self.grid[y][x] = 0
-            
-        def world_to_grid(self, x, y):
-            return (x // self.cell_width, y // self.cell_height)
+    def toggle_obstacle(self, y, x):
+        if self.grid[y][x] == 0:
+            self.grid[y][x] = 1
+        else:
+            self.grid[y][x] = 0
+        
+    def world_to_grid(self, y, x):
+        return (y // self.cell_height, x // self.cell_width)
 
-        def grid_to_world(self, x, y):
-            return (x * self.cell_width, y * self.cell_height)
+    def grid_to_world(self, y, x):
+        return (y * self.cell_height, x * self.cell_width)
 
-        def get_grid(self):
-            return self.grid
+    def get_grid(self):
+        return self.grid
