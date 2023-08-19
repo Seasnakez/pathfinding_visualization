77645d1a5e804cadb33e4fbee9c8ac5aa6e793b1
Seasnakez <adde456789@gmail.com>
2023-08-19 22:22:49 +0200

    Added visualizer

---
+++ AStarVisualizer.py
@@ -0,0 +1,6 @@
+import pygame as pg
+
+class AStarVisualizer:
+    def __init__(self, a_star_instance):
+        self.a_star_instance = a_star_instance
+        
--- ObstacleGrid.py
+++ ObstacleGrid.py
@@ -11,11 +11,11 @@ class ObstacleGrid:
             else:
                 self.grid[y][x] = 0
             
-        def world_to_grid(x, y):
-            pass
+        def world_to_grid(self, x, y):
+            return (x // self.cell_width, y // self.cell_height)
 
-        def grid_to_world(x, y):
-            pass
+        def grid_to_world(self, x, y):
+            return (x * self.cell_width, y * self.cell_height)
 
         def get_grid(self):
             return self.grid
