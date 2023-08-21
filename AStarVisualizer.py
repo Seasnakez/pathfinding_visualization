9c063a1fc710064bc6b411ed57d8e0f3e966fdf6
Seasnakez <adde456789@gmail.com>
2023-08-21 12:39:38 +0200

    Added draw() to ObstacleGrid

--- AStarVisualizer.py
+++ AStarVisualizer.py
@@ -1,21 +1,15 @@
+from ObstacleGrid import *
+from AStar import *
 import pygame as pg
 
 class AStarVisualizer:
-    def __init__(self, render, a_star, grid):
+    def __init__(self, render):
         self.render = render
-        self.a_star = a_star
-        self.grid = grid
+        self.a_star = AStar()
+        self.grid = ObstacleGrid(render, 3, 5, 100, 100)
+
+        # Adds obstacles
+        self.grid.toggle_obstacle(0, 1)
 
     def draw(self):
-        # Draw filled
-        for y, row in enumerate(self.grid.get_grid()):
-            for x, has_obstacle in enumerate(row):
-                if has_obstacle:
-                    color = pg.Color("grey")
-                else:
-                    color = pg.Color("white")
-                pg.draw.rect(self.render.screen, color, pg.Rect(x * self.grid.cell_width, y * self.grid.cell_height, self.grid.cell_width, self.grid.cell_height))
-                pg.draw.rect(self.render.screen, pg.Color("darkgray"), pg.Rect(x * self.grid.cell_width, y * self.grid.cell_height, self.grid.cell_width, self.grid.cell_height), 1)
-        
-        # All borders but the edges get drawn twice, to keep all borders equal size, a thicker border is drawn on the edges to compensate
-        pg.draw.rect(self.render.screen, pg.Color("darkgray"), pg.Rect(0, 0, self.grid.width * self.grid.cell_width, self.grid.height * self.grid.cell_height), 2)
+        self.grid.draw()
--- ObstacleGrid.py
+++ ObstacleGrid.py
@@ -1,6 +1,10 @@
+import pygame as pg
+
 class ObstacleGrid:
-    def __init__(self, height, width, cell_width, cell_height):
-        self.grid = [[0 for _ in range(width)] for _ in range(height)]
+    def __init__(self, render, height, width, cell_width, cell_height):
+        self.render = render
+        self.obstacle_map = [[0 for _ in range(width)] for _ in range(height)]
+        self.color_map = [[pg.Color("white") for _ in range(width)] for _ in range(height)]
         self.cell_width = cell_width
         self.cell_height = cell_height
         self.height = height
@@ -9,17 +13,30 @@ class ObstacleGrid:
     def adjacent(self, include_obstacles=True):
         pass
 
-    def toggle_obstacle(self, y, x):
-        if self.grid[y][x] == 0:
-            self.grid[y][x] = 1
+    def toggle_obstacle(self, row, column):
+        if self.obstacle_map[row][column] == 0:
+            self.obstacle_map[row][column] = 1
+            self.color_map[row][column] = pg.Color("gray")
         else:
-            self.grid[y][x] = 0
+            self.obstacle_map[row][column] = 0
+            self.color_map[row][column] = pg.Color("white")
         
-    def world_to_grid(self, y, x):
+    def world_to_grid(self, x, y):
         return (y // self.cell_height, x // self.cell_width)
 
-    def grid_to_world(self, y, x):
-        return (y * self.cell_height, x * self.cell_width)
+    def grid_to_world(self, row, column):
+        return (column * self.cell_width, row * self.cell_height)
 
     def get_grid(self):
-        return self.grid
+        return self.obstacle_map
+
+    def draw(self):
+        for y, row in enumerate(self.color_map):
+            for x, color in enumerate(row):
+                # Draw filled square
+                pg.draw.rect(self.render.screen, color, pg.Rect(x * self.cell_width, y * self.cell_height, self.cell_width, self.cell_height))
+                # Draw border
+                pg.draw.rect(self.render.screen, pg.Color("darkgray"), pg.Rect(x * self.cell_width, y * self.cell_height, self.cell_width, self.cell_height), 1)
+        
+        # All borders but the edges get drawn twice, to keep all borders equal size, a thicker border is drawn on the edges to compensate
+        pg.draw.rect(self.render.screen, pg.Color("darkgray"), pg.Rect(0, 0, self.width * self.cell_width, self.height * self.cell_height), 2)
