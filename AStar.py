4a07f8dd7d54f4091e422f50e8c95d5245745e3a
Seasnakez <adde456789@gmail.com>
2023-08-21 23:10:12 +0200

    Handled the case where there is no valid path to the goal

--- AStar.py
+++ AStar.py
@@ -21,43 +21,44 @@ class AStar:
         self.closed = []
 
     def step(self):
-        # Picks lowest cost node in open
-        current = self.open[0]
-        for node in self.open:
-            if node.cost() < current.cost():
-                current = node
-
         # If open set is empty, there is no valid path to the goal
-        if not self.open: path = [] 
-        else: path = None
-        if current.position == self.goal:
-            path = self.reconstruct(current)
-            print(path)
-        else:
-            self.open.remove(current)
-            self.closed.append(current)
-            for neighbour in self.get_neighbours(*current.position):
-                cost = current.g + 1
+        path = [self.start]
+        if self.open:
+            # Picks lowest cost node in open
+            current = self.open[0]
+            for node in self.open:
+                if node.cost() < current.cost():
+                    current = node
+            
+            path = None
+            if current.position == self.goal:
+                path = self.reconstruct(current)
+                print(path)
+            else:
+                self.open.remove(current)
+                self.closed.append(current)
+                for neighbour in self.get_neighbours(*current.position):
+                    cost = current.g + 1
 
-                in_open = False
-                in_closed = False
+                    in_open = False
+                    in_closed = False
 
-                # if neighbour already exists in open and is more expensive, it's inferior and is removed
-                node = self.get_by_position(neighbour, self.open)
-                if node:
-                    in_open = True
-                    if cost < node.g:
-                        self.open.remove(node)
-                # if neighbour already exists in closed and is more expensive, it's inferior and is removed
-                node = self.get_by_position(neighbour, self.closed)
-                if node:
-                    in_closed = True
-                    if cost < node.g:
-                        self.closed.remove(node)
-                
-                if not in_open and not in_closed:
-                    new_node = AStarNode(cost, self.cost_heuristic(neighbour), current, neighbour)
-                    self.open.append(new_node)
+                    # if neighbour already exists in open and is more expensive, it's inferior and is removed
+                    node = self.get_by_position(neighbour, self.open)
+                    if node:
+                        in_open = True
+                        if cost < node.g:
+                            self.open.remove(node)
+                    # if neighbour already exists in closed and is more expensive, it's inferior and is removed
+                    node = self.get_by_position(neighbour, self.closed)
+                    if node:
+                        in_closed = True
+                        if cost < node.g:
+                            self.closed.remove(node)
+                    
+                    if not in_open and not in_closed:
+                        new_node = AStarNode(cost, self.cost_heuristic(neighbour), current, neighbour)
+                        self.open.append(new_node)
                 
         return self.convert_to_positions(self.open), self.convert_to_positions(self.closed), path
     
@@ -74,6 +75,7 @@ class AStar:
         return None
 
     def cost_heuristic(self, position):
+        return 0
         return math.sqrt((position[0] - self.goal[0])**2 + (position[1] - self.goal[1])**2) * 10
 
     def reconstruct(self, node):
